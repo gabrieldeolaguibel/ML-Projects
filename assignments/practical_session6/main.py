4ad0f627a4e13370b4db3d83644b574763a40322
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2024-03-11 22:51:55 +0100

    Update YOLO model instantiation in main.py

--- assignments/practical_session6/main.py
+++ assignments/practical_session6/main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load the model
-model = YOLO("yolov8n.yaml") # build from scratch
+model = YOLO("yolov8n.yaml") # build from scratch using the nano model
 
 # train the model
 results = model.train(data="config.yaml", epochs=100)
