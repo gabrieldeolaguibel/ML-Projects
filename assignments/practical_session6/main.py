3e69bb9520b697b6383cd737dc6dbc2366665f99
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2024-03-12 20:44:41 +0100

    Added Notes

--- assignments/practical_session6/main.py
+++ assignments/practical_session6/main.py
@@ -5,3 +5,5 @@ model = YOLO("yolov8n.yaml") # build from scratch using the nano model
 
 # train the model
 results = model.train(data="config.yaml", epochs=100)
+# you can view the training validation results in the runs/detect/train folder and see the 'val_batch0_pred.jpg' file to see the results of the model on the validation set
+
